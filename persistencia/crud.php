92718d7601b74b55163e392908eb477c49a61b32
Francisco Fourcade <f.fourcade@itecriocuarto.org.ar>
2021-01-15 14:37:28 -0300

    started crud script

---
+++ persistencia/crud.php
@@ -0,0 +1,22 @@
+<?PHP
+
+  class Crud {
+
+    protected $tabla;
+    protected $conexion;
+    protected $wheres;
+    protected $sql = null;
+
+    public function __construct($tabla = null){
+      $this -> conexion = (new Conexion()) -> conectar();
+      $this -> tabla = $tabla;  
+    }
+    {
+      
+    }
+
+
+  }
+
+?>
+
